d4a3156c855d0cfb3c5bc86bf1a7f06eea334adb
Jeanndo <ukjeando@gmail.com>
2021-09-24 21:02:28 +0200

    updated product modal

--- client/src/components/AllItems/Modal.jsx
+++ client/src/components/AllItems/Modal.jsx
@@ -1,31 +1,30 @@
-import Button from '@mui/material/Button';
-import Modal from '@mui/material/Modal';
+import Button from "@mui/material/Button";
+import Modal from "@mui/material/Modal";
 import AddBoxIcon from "@mui/icons-material/AddBox";
-import AddItem from '../AddItem/AddItem';
+import AddItem from "../AddItem/AddItem";
 
-
-const AddItemModal = ({handleOpen,open,handleClose})=>{
-    return (
-        <div>
-        <Button onClick={handleOpen} style={{float:'left'}} variant="outlined">
-            <AddBoxIcon
-            style={{
-              cursor: "pointer",
-              float: "left",
-              color: "blue",
-            }}
-          />
-          Add new product
-          </Button>
-        <Modal
-          open={open}
-          onClose={handleClose}
-          aria-labelledby="modal-modal-title"
-          aria-describedby="modal-modal-description"
-        >
-          <AddItem handleClose={handleClose}/>
-        </Modal >
-      </div>
-    )
-}
-export default AddItemModal
+const AddItemModal = ({ handleOpen, open, handleClose }) => {
+  return (
+    <div>
+      <Button onClick={handleOpen} style={{ float: "left" }} variant="outlined">
+        <AddBoxIcon
+          style={{
+            cursor: "pointer",
+            float: "left",
+            color: "blue",
+          }}
+        />
+        Add new product
+      </Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <AddItem handleClose={handleClose} />
+      </Modal>
+    </div>
+  );
+};
+export default AddItemModal;
