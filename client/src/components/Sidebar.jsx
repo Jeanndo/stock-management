8a1fa61bf3645ab5f732d46f26795ceb4d177bb8
Jeanndo <ukjeando@gmail.com>
2021-09-24 21:05:10 +0200

    added user avatar on sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -2,78 +2,55 @@ import React from "react";
 import ListItem from "@mui/material/ListItem";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import StoreIcon from '@mui/icons-material/Store';
-import AddBusinessIcon from '@mui/icons-material/AddBusiness';
-import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import StoreIcon from "@mui/icons-material/Store";
 import BarChartIcon from "@mui/icons-material/BarChart";
-import VisibilityIcon from '@mui/icons-material/Visibility';
-import AddItem from "./AddItem/AddItem";
+import VisibilityIcon from "@mui/icons-material/Visibility";
 import AllItems from "./AllItems/AllItems";
+import AllUsers from "./AllUsers/AllUsers";
+import Stock from './stock/stock'
+import { PeopleTwoTone } from "@mui/icons-material";
+import Avatar from '@mui/material/Avatar';
+import Divider from "@mui/material/Divider"
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 const Sidebar = (props) => {
- 
   const handleDashboard = () => {
-    props.setContents('Dasboard')
+    props.setContents(<Stock/>);
   };
 
-  const handleAddItem= () => {
-    props.setContents(<AddItem/>)
-  }
+  const handleAllItems = () => {
+    props.setContents(<AllItems />);
+  };
 
-  const handleAddUser = ()=>{
-    props.setContents('Adduser')
-  }
-  const handleAllItems = ()=>{
-    props.setContents(<AllItems/>)
-  }
-
-  const handleAllUsers = ()=>{
-    props.setContents('All users')
-  }
-const handleReort = ()=>{
-  props.setContents('Report')
-}
-const handleAllClients =()=>{
-  props.setContents('All users')
-}
-
+  const handleAllUsers = () => {
+    props.setContents(<AllUsers />);
+  };
+  const handleReort = () => {
+    props.setContents("Report");
+  };
   return (
     <div>
+      <div style={{marginLeft:'10px',marginBottom:'10px'}}>
+      <Avatar alt="user" src="https://avatars.githubusercontent.com/u/59208992?v=4" />
+      </div>
+      <Divider/>
       <ListItem button onClick={handleDashboard}>
         <ListItemIcon>
-          <StoreIcon/>
+          <StoreIcon />
         </ListItemIcon>
         <ListItemText primary="Stock" />
       </ListItem>
-      <ListItem button onClick={handleAddItem}>
+      <ListItem button onClick={handleAllItems}>
         <ListItemIcon>
-          <AddBusinessIcon />
+          <ShoppingCartIcon />
         </ListItemIcon>
-        <ListItemText primary="Add Item" />
-      </ListItem>
-      <ListItem button onClick={handleAddUser}>
-        <ListItemIcon>
-          <PersonAddIcon />
-        </ListItemIcon>
-        <ListItemText primary="Add User" />
+        <ListItemText primary="All Products" />
       </ListItem>
       <ListItem button onClick={handleAllUsers}>
         <ListItemIcon>
-          <VisibilityIcon />
+          <PeopleTwoTone />
         </ListItemIcon>
-        <ListItemText primary="View All Users" />
-      </ListItem>
-      <ListItem button onClick={handleAllItems}>
-        <ListItemIcon>
-          <VisibilityIcon />
-        </ListItemIcon>
-        <ListItemText primary="View All Items" />
-      </ListItem>
-      <ListItem button onClick={handleAllClients}>
-        <ListItemIcon>
-          <VisibilityIcon />
-        </ListItemIcon>
-        <ListItemText primary="View All Clients" />
+        <ListItemText primary="All Users" />
       </ListItem>
       <ListItem button onClick={handleReort}>
         <ListItemIcon>
@@ -81,7 +58,6 @@ const handleAllClients =()=>{
         </ListItemIcon>
         <ListItemText primary="Report" />
       </ListItem>
-      
     </div>
   );
 };
