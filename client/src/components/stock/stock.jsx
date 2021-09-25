695eebde6b3f96c76da8420576dd1773fea9305c
Jeanndo <ukjeando@gmail.com>
2021-09-25 19:07:24 +0200

    modified

--- client/src/components/stock/stock.jsx
+++ client/src/components/stock/stock.jsx
@@ -1,4 +1,3 @@
-import { Label } from "@mui/icons-material";
 import { useStyles } from "./style"
 
 const Stock = () => {
@@ -11,7 +10,7 @@ const Stock = () => {
              <div className={classes.stockUpperPart}>
                  <div className={classes.users}>
                  <h2 className={classes.heading1}>123444</h2>
-                <label style={{color:'white'}}>USERS</label>
+                   <label style={{color:'white'}}>USERS</label>
                  </div>
                  <div className={classes.Products}>
                  <h2 className={classes.heading1}>123521</h2>
