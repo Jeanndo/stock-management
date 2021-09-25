1f8144b91840be149b2f691b105788edc667df8c
Jeanndo <ukjeando@gmail.com>
2021-09-25 19:04:47 +0200

    added sign up modal

--- client/src/components/AddUser/AddUser.jsx
+++ client/src/components/AddUser/AddUser.jsx
@@ -1,50 +1,147 @@
-import { useStyles } from "./styles";
-import { Button } from "@mui/material";
+import * as React from "react";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import { Link } from "react-router-dom";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import GoogleLogin from "react-google-login";
+import {useStyles} from './styles'
 
-const AddItem = ({handleClose}) => {
-  const classes = useStyles();
+
+const theme = createTheme();
+
+export default function SignUp() {
+
+  const classes = useStyles()
+
   const handleSubmit = (event) => {
     event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    // eslint-disable-next-line no-console
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+  };
+
+  const googleSuccess = async (res) => {
+    //   const result = res?.profileObj;
+    //   const token = res?.tokenId;
+    // try {
+    //     dispatch({type:'AUTH',data:{result,token}});
+    //     history.push('/');
+    // } catch (error) {
+    //    console.log(error);
+    // }
+  };
+  const googleFailure = (error) => {
+    console.log(error);
+    console.log("Google Sing In was unsuccessful. Try Again Later");
   };
   return (
-    <div className={classes.additemContainer}>
-      <form autoComplete="off" onSubmit={handleSubmit}>
-        <h2 className={classes.addNewProduct}>Add New User</h2>
-        <input
-          type="text"
-          placeholder="First Name"
-          className={classes.ProductName}
-        />
-        <input
-          type="text"
-          placeholder="Last Name"
-          className={classes.Quantity}
-        />
-        <input
-          type="number"
-          placeholder="Phone"
-          className={classes.Phone}
-        />
-        <input type="text" placeholder="Role" className={classes.Price} />
-        <Button
-          variant="outlined"
-          color="primary"
-          className={classes.CancelBtn}
-          onClick={handleClose}
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs" className={classes.AuthBack} >
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
         >
-          Cancel
-        </Button>
-        <Button
-          variant="outlined"
-          color="primary"
-          type="submit"
-          className={classes.AddBtn}
-        >
-          Add
-        </Button>
-      </form>
-    </div>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+            sx={{ mt: 3 }}
+          >
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="fname"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="lname"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="outlined"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <GoogleLogin
+              clientId="65688288942-pf0sgh5gcvnneskbgspl6mel27bbuf91.apps.googleusercontent.com"
+              render={(renderProps) => (
+                <Button
+                  variant="outlined"
+                  color="primary"
+                  fullWidth
+                  onClick={renderProps.onClick}
+                  disabled={renderProps.deisabled}
+                >
+                  Google Sign In
+                </Button>
+              )}
+              onSuccess={googleSuccess}
+              onFailure={googleFailure}
+              cookiePolicy="single_host_origin"
+            />
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link to="/login" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
   );
-};
-
-export default AddItem;
+}
