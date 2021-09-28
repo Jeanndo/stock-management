4b62edda19d147cf1fc6808d3924cc0d116e209f
Jeanndo <jeanndo@pop-os.localdomain>
2021-09-28 14:20:49 +0200

    implemented authentication middleware for resource protection

---
+++ server/middlewares/auth.js
@@ -0,0 +1,24 @@
+import jwt from 'jsonwebtoken';
+
+const auth = async(req,res,next)=>{
+    try {
+        // console.log(req.headers);
+    const token =  req.headers.authorization.split(" ")[1];   
+    // const isCustomAuth = token.length <500;
+    
+    let decodedData;
+
+        decodedData = jwt.decode(token);
+
+        req.userId = decodedData?.sub;
+        //sub in a google name for any user google user id
+
+    
+
+    next();
+} catch (error) {
+        console.log(error);
+    }
+}
+
+export default auth;
