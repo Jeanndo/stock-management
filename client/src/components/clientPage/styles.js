6526945e4219580b30be1822ce9dfaba59d0bb45
Jeanndo <jeanndo@pop-os.localdomain>
2021-09-29 22:31:24 +0200

    added new styles

---
+++ client/src/components/clientPage/styles.js
@@ -0,0 +1,33 @@
+import {makeStyles} from '@mui/styles'
+
+export const useStyles = makeStyles({
+navContainer:{
+    marginTop:'-10px',
+    width:'100%',
+    marginBottom:'300px',
+    textAlign:'center',
+    display:'flex',
+    flexDirection:'row',
+    color:'#fff',
+    height:'10vh',
+    background: "#3f51b5",
+    position:'fixed'
+    
+},
+welcome:{
+    width:'80%',
+    padding:'10px'
+},
+profile:{
+    float:'right',
+    width:'200px',
+    
+},
+payment:{
+    marginLeft:'2%',
+    width:'95%',
+    color:'#fff',
+    height:'10vh',
+    background: "#3f51b5",
+}
+})
