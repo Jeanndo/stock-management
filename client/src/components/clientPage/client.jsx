f236806fe040ffd325f22ff8fec0e58303615e44
Jeanndo <jeanndo@pop-os.localdomain>
2021-09-29 22:24:03 +0200

    implemented client ui page

--- client/src/components/clientPage/client.jsx
+++ client/src/components/clientPage/client.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
-
+import CustomerProducts from './userTable'
+import Navigation from './Navbar'
+import Payment from './payments'
 const Client =()=>{
 
     return (
         <div>
-            client page
+            <Navigation/>
+            <br/> <br/> <br/>
+            <Payment/>
+            <br/> <br/> <br/> <br/>
+            <CustomerProducts/>
             </div>
     )
 }
